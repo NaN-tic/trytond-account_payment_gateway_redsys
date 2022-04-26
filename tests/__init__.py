c00650d1a96de85ff061f24bee7a417103bdb471
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:12:31 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# This file is part account_payment_gateway_redsys module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-try:
-    from trytond.modules.account_payment_gateway_redsys.tests.test_account_payment_gateway_redsys import suite
-except ImportError:
-    from .test_account_payment_gateway_redsys import suite
-
-__all__ = ['suite']
